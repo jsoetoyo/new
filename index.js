dd5dd0c8d4daf83b3fba959c8aa23606211ddd89
jeth <jeth.soetoyo@gmail.com>
2017-12-25 00:09:25 -0500

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+//To load express library to the variable express
+const express=require("express");
+//TO initiate express, so () asks node.js to execute the library
+const app=express();
+// To tell index.js to go to the path folder of public and run it. First create the constant
+const path = require('path');
+
+//to open up the folder by joinng the main directory name and the public folder
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Start the server by connecting to the port:
+const server = app.listen(3000);
+console.log(server.address().port);
